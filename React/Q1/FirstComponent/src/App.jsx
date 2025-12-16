8a8eb4f5f9559a999c04b59d0aff97970abd5f0b
Ojaswini <ojaswini.volukula@gmail.com>
2025-12-16 16:27:11 +0530

    Created FirstComponent

---
+++ React/Q1/FirstComponent/src/App.jsx
@@ -0,0 +1,10 @@
+// import { useState } from 'react'
+import FirstComponent  from './FirstComponent.jsx'
+function App(){
+  return (
+    <div>
+      <FirstComponent/>
+    </div>
+  );
+}
+export default App;
---
+++ React/Q1/FirstComponent/src/FirstComponent.jsx
@@ -0,0 +1,6 @@
+function FirstComponent(){
+    return (
+    <h2>This is my first react component</h2>
+    );
+}
+export default FirstComponent;
