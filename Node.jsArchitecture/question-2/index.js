746a24e8e3d67a9819ec29fc0c67b1a829e98f06
Ojaswini <ojaswini.volukula@gmail.com>
2026-01-09 13:14:08 +0530

    custom module

---
+++ Node.jsArchitecture/question-2/index.js
@@ -0,0 +1,6 @@
+import { checkPrime } from "./math.js";
+console.log(checkPrime(2))
+console.log(checkPrime(5))
+console.log(checkPrime(49))
+console.log(checkPrime(37))
+console.log(checkPrime(54))
